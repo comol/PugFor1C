318a16b861c870de17856271af7f09fce05785c1
comol <comol@mail.ru>
2020-02-27 00:58:21 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,38 @@
+const pug = require('pug');
+const fs = require('fs');
+const path = require('path');
+const http = require("http");
+
+http.createServer(function (request, response) {
+
+    let data = [];
+
+    request.on('data', chunk => {
+        data.push(chunk)
+    });
+
+    request.on('end', () => {
+        var parameters = JSON.parse(data);
+
+        if (parameters.jsondata == "ping")
+        {
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end("OK");
+            return;
+        }
+	// синхронно осознанно. В 1С обработка синхронная, в текущей версии движка
+        let jsonb = {};
+        fs.readdirSync(parameters.jsondata).forEach((file) => {
+            let jsonobj = JSON.parse(fs.readFileSync(path.join(parameters.jsondata, file), 'utf8'));
+            Object.assign(jsonb, jsonobj);
+        });
+
+        fs.writeFileSync(parameters.page, pug.renderFile(parameters.template, jsonb));
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end();
+    })
+}).listen(8282);
+
+
+
+
